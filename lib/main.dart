242c13abc425054aba9a95ed08421155331630b6
Aleksander Jess <jess.aleksander@gmail.com>
2021-12-22 22:38:36 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import "dart:async";
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:async/async.dart';
+// import 'package:geolocator/geolocator.dart';
+// import 'package:http/http.dart';
+// import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Future Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  @override
+  _FuturePageState createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String? result;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back from the Future'),
+      ),
+      body: Center(
+        child: Column(children: [
+          const Spacer(),
+          ElevatedButton(
+            child: const Text('GO!'),
+            onPressed: () {},
+          ),
+          const Spacer(),
+          Text(result.toString()),
+          const Spacer(),
+          const CircularProgressIndicator(),
+          const Spacer(),
+        ]),
+      ),
+    );
+  }
+}
